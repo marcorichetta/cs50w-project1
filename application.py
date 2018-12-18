bcc93c763e5246f2fd7f981cae48d11dff7f83b5
marcorichetta <marcorichetta@gmail.com>
2018-12-18 12:38:28 -0300

    Error and results templates created. SQL query for results made. Grid result page.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, redirect, render_template, request
+from flask import Flask, session, redirect, render_template, request, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -34,7 +34,7 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     """ Show search box """
 
-    return render_template("layout.html")
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -50,13 +50,14 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return ("must provide username", 403)
+            return ("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return ("must provide password", 403)
+            return ("must provide password", 400)
 
         # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
+        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                             {"username": username})
         
@@ -64,12 +65,12 @@ def login():
 
         # Ensure username exists and password is correct
         if result == None or not check_password_hash(result[2], request.form.get("password")):
-            return ("invalid username and/or password", 403)
+            return ("invalid username and/or password", 400)
 
         # Remember which user has logged in
         session["user_id"] = result[0]
         session["user_name"] = result[1]
-        
+
         # Redirect user to home page
         return redirect("/")
 
@@ -99,7 +100,7 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return ("must provide username", 403)
+            return ("must provide username", 400)
 
         # Query database for username
         userCheck = db.execute("SELECT * FROM users WHERE username = :username",
@@ -107,19 +108,19 @@ def register():
 
         # Check if username already exist
         if userCheck:
-            return ("username already exist", 403)
+            return ("username already exist", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return ("must provide password", 403)
+            return ("must provide password", 400)
 
         # Ensure confirmation wass submitted 
         elif not request.form.get("confirmation"):
-            return ("must confirm password", 403)
+            return ("must confirm password", 400)
 
         # Check passwords are equal
         elif not request.form.get("password") == request.form.get("confirmation"):
-            return ("passwords didn't match", 403)
+            return ("passwords didn't match", 400)
         
         # Hash user's password to store in DB
         hashedPassword = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
@@ -138,3 +139,37 @@ def register():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
+
+@app.route("/search", methods=["GET"])
+@login_required
+def search():
+    """ Get books results """
+
+    # Check book id was provided
+    if not request.args.get("book"):
+        return render_template("error.html", message="you must provide a book.")
+
+    # Take input and add a wildcard
+    query = "%" + request.args.get("book") + "%"
+
+    # Capitalize all words of input for search
+    # https://docs.python.org/3.7/library/stdtypes.html?highlight=title#str.title
+    query = query.title()
+    
+    rows = db.execute("SELECT isbn, title, author, year FROM books WHERE \
+                        isbn LIKE :query OR \
+                        title LIKE :query OR \
+                        author LIKE :query LIMIT 15",
+                        {"query": query})
+    
+    if rows.rowcount == 0:
+        return render_template("error.html", message="we can't find books with that id.")
+    
+    books = rows.fetchall()
+
+    """ TODO 
+    Use Openlibrary Cover API
+    http://covers.openlibrary.org/b/isbn/2266079999-M.jpg
+    """
+
+    return render_template("results.html", books=books)
